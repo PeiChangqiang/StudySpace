3c901ee53a44123ff6a5825a110219c44817d617
peichangqiang <1156368718@qq.com>
2018-09-28 13:58:33 +0800

    增加Cglib代理类和责任链demo的java代码

--- Java/SystemStudy/src/com/pcq/dynamicproxy/CglibProxy.java
+++ Java/SystemStudy/src/com/pcq/dynamicproxy/CglibProxy.java
@@ -1,19 +1,57 @@
 /*
+
 package com.pcq.dynamicproxy;
 
-import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
 
 */
 /**
  * CGLIB代理类
  *//*
 
-public class CglibProxy implements org.aopalliance.intercept.MethodInterceptor {
-    @Override
-    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
-        return null;
+
+public class CglibProxy implements org.springframework.cglib.proxy.MethodInterceptor {
+
+
+    */
+/**
+     * 生成代理对象
+     * @param c 被代理的类
+     * @return
+     *//*
+
+    public Object getProxy(Class c) {
+        //增强类对象
+        org.springframework.cglib.proxy.Enhancer enhancer = new Enhancer();
+        //设置增强类型
+        enhancer.setSuperclass(c);
+        //代理逻辑对象设置为当前对象,该对象的类必须实现了MethodInterceptor的intercept方法
+        enhancer.setCallback(this);
+        //生成和返回代理对象
+        return enhancer.create();
     }
 
+    */
+/**
+     *
+     * @param o 代理对象
+     * @param method 方法
+     * @param objects 方法参数
+     * @param methodProxy 方法代理
+     * @return 代理的逻辑返回
+     * @throws Throwable
+     *//*
 
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.err.println("调用代理类方法前执行逻辑...");
+        Object result = methodProxy.invokeSuper(o, objects);
+        System.err.println("调用后...");
+        return result;
+    }
 }
+
 */
--- Java/SystemStudy/src/com/pcq/dynamicproxy/CommonTest.java
+++ Java/SystemStudy/src/com/pcq/dynamicproxy/CommonTest.java
@@ -17,4 +17,20 @@ public class CommonTest {
         MakeDinner proxy = (MakeDinner) InterceptorJdkProxy.bind(new MakeDinnerImpl(), "com.pcq.dynamicproxy.MyInterceptor");
         proxy.makeDinner();
     }
+
+    @Test
+    public void testDutyChain() {
+        MakeDinner proxy1 = (MakeDinner) InterceptorJdkProxy.bind(new MakeDinnerImpl(), "com.pcq.dynamicproxy.Interceptor1");
+        MakeDinner proxy2 = (MakeDinner) InterceptorJdkProxy.bind(proxy1, "com.pcq.dynamicproxy.Interceptor2");
+        MakeDinner proxy3 = (MakeDinner) InterceptorJdkProxy.bind(proxy2, "com.pcq.dynamicproxy.Interceptor3");
+        proxy3.makeSoup("筒骨汤");
+
+    }
+
+  /*  @Test
+    public void testCglibProxy() {
+        CglibProxy cProxy = new CglibProxy();
+        MakeDinnerImpl md = (MakeDinnerImpl) cProxy.getProxy(MakeDinnerImpl.class);
+        md.makeSoup("排骨汤");
+    }*/
 }
