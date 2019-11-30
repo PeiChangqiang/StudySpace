ff3156c5b97c27b7340d02e8036910e32998bf60
peichangqiang <1156368718@qq.com>
2019-11-30 09:07:25 +0800

    删除历史mybatis项目

--- Java/Mybatis/base_module/src/main/java/com/pcq/mapper/StudentMapper2.java
+++
@@ -1,9 +0,0 @@
-package com.pcq.mapper;
-
-import com.pcq.entity.Student;
-import org.apache.ibatis.annotations.Select;
-
-public interface StudentMapper2 {
-    @Select("SELECT sId,sName,sAge,sSex FROM student WHERE sId = #{id}")
-    Student findStudentById(String sId);
-}
