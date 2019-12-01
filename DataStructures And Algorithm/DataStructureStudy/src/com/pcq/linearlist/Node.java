c64dbaf864705f83b5c5ce94a673e1d5d503fb2b
peichangqiang <1156368718@qq.com>
2019-12-01 22:27:28 +0800

    链表操作代码

---
+++ DataStructures And Algorithm/DataStructureStudy/src/com/pcq/linearlist/Node.java
@@ -0,0 +1,45 @@
+package com.pcq.linearlist;
+
+/**
+ * 链表节点类
+ * @author Administrator
+ *
+ */
+public class Node {
+
+	/**
+	 * 数据域
+	 */
+	private int data;
+	
+	/**
+	 * 指针域
+	 */
+	private Node next;
+	
+	public Node(int data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+	
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+}
