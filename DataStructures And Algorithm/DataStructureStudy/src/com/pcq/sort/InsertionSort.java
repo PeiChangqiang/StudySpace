c7655a8a1d8e2fc982ba692ac62e26467f79a81f
peichangqiang <1156368718@qq.com>
2019-12-05 18:02:01 +0800

    更新笔记

--- DataStructures And Algorithm/DataStructureStudy/src/com/pcq/sort/InsertionSort.java
+++ DataStructures And Algorithm/DataStructureStudy/src/com/pcq/sort/InsertionSort.java
@@ -7,7 +7,7 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] array = ArrayUtil.getArray(10);
 		ArrayUtil.printArray(array);
-		insertionSort (array);
+		insertionSort2 (array);
 		ArrayUtil.printArray(array);
 
 	}
@@ -28,5 +28,24 @@ public class InsertionSort {
 				} 
 			}
 		}
+	}
+	
+	
+	public static void insertionSort2(int[] array) {
+		if(array == null || array.length == 0) 
+			return;
+		for(int i = 1; i < array.length; ++i) {//默认从1开始，因为最开始第一个元素是有序区间
+			int value = array[i];
+			int j = i-1;
+			for(; j >= 0; --j) {
+				if(value < array[j]) {
+					array[j+1] = array[j];
+				} else {
+					break;
+				}
+			}
+			array[j+1] = array[i];//插入数据
+		}
+		
 	}
 }
