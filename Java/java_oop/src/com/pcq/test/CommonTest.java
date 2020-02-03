520018d4f6a862a156088602b721603dcbbaefcf
peichangqiang <1156368718@qq.com>
2020-02-03 11:25:02 +0800

    更新代码

---
+++ Java/java_oop/src/com/pcq/test/CommonTest.java
@@ -0,0 +1,15 @@
+package com.pcq.test;
+import org.junit.*;
+public class CommonTest {
+
+	public CommonTest() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	
+	
+	@Test
+	public void testInteger() {
+		System.out.println(Integer.toHexString(2658));
+	}
+}
