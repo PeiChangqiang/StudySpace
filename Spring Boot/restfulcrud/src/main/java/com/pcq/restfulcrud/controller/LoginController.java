61950687a05ccfb0d291d22085aa22a290bbaed4
peichangqiang <1156368718@qq.com>
2020-07-13 09:50:39 +0800

    提交SpringBoot笔记和代码

--- Spring Boot/restfulcrud/src/main/java/com/pcq/restfulcrud/controller/LoginController.java
+++ Spring Boot/restfulcrud/src/main/java/com/pcq/restfulcrud/controller/LoginController.java
@@ -1,4 +1,30 @@
 package com.pcq.restfulcrud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+
+@Controller
 public class LoginController {
+
+    @PostMapping("/user/login")
+    //@RequestMapping(value="/user/login", method= RequestMethod.POST)
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String,Object> map, HttpSession session) {
+    if(!StringUtils.isEmpty(username) && "123456".equals(password)) {//登陆成功
+        session.setAttribute("loginUser", username);
+        //防止表单重复提交，则重定向到成功页面
+
+        return "redirect:/succeed.html";
+    }
+    //登陆失败
+        map.put("message", "用户名或者密码错误！");
+        return "logging";
+    }
 }
--- Spring Boot/restfulcrud/src/main/java/com/pcq/restfulcrud/controller/ThymeleafController.java
+++ Spring Boot/restfulcrud/src/main/java/com/pcq/restfulcrud/controller/ThymeleafController.java
@@ -4,12 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class ThymeleafController {
 
 
     @RequestMapping("/success")
-    public String sayHello() {
+    public String sayHello(Map<String, Object> map) {
+        map.put("success", "成功");
+        map.put("list", Arrays.asList("张三", "李四", "王五"));
         return "success";
+    }
+
+
+    @RequestMapping("/logging")
+    public String logging() {
+        return "logging";
     }
 }
