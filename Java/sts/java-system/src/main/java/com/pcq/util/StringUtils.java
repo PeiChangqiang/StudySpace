192482be23a35172a6ac22cc94d7cd3a3794affa
peichangqiang <1156368718@qq.com>
2021-01-15 00:09:29 +0800

    do nothing again

--- Java/sts/java-system/src/main/java/com/pcq/util/StringUtils.java
+++ Java/sts/java-system/src/main/java/com/pcq/util/StringUtils.java
@@ -3,6 +3,6 @@ package com.pcq.util;
 public class StringUtils {
 
 	public static void main(String[] args) {
-		System.out.println("do nothing");
+		System.out.println("do nothing and just test");
 	}
 }
